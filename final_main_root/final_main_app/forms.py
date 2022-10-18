1df099570d82191269259815130b6a60e24cc238
Alexeia <alexeiaw@gmail.com>
2022-10-18 11:32:43 +0200

    group django form model

--- final_main_root/final_main_app/forms.py
+++ final_main_root/final_main_app/forms.py
@@ -3,20 +3,29 @@ from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django import forms
 
+
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
-        fields = ('username', 'first_name' , 'last_name' , 'email', 'password')\
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')\
+
 
 
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username', 'first_name' , 'last_name' , 'email')
+        fields = ('username', 'first_name', 'last_name', 'email')
+
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = AppUser
         fields = ('organisation', 'status')
+
+
+class GroupForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = ('name', 'description')
