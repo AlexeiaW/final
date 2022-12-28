ee5635758614b72d041d3a91cfaac2e25625d13b
Alexeia <alexeiaw@gmail.com>
2022-12-28 17:04:08 +0200

    username in chat

--- final_main_root/final_main_app/consumers.py
+++ final_main_root/final_main_app/consumers.py
@@ -3,10 +3,12 @@ import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
+
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
+        self.user = self.scope["user"]
 
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
@@ -33,15 +35,18 @@ class ChatConsumer(WebsocketConsumer):
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'username': self.user.username
             }
         )
 
     # Receive message from room group
     def chat_message(self, event):
         message = event['message']
+        username = event['username']
 
         # Send message to WebSocket
         self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'username': username,
         }))
